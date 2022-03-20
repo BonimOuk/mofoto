7295ad02bbe476f7c299e0f7d44b07482835bf25
Bonim <bonimouk@gmail.com>
2022-03-19 18:31:21 -0700

    Uploading To Cloudinary Basics

--- routes/mofotos.js
+++ routes/mofotos.js
@@ -4,7 +4,8 @@ const mofotos = require('../controllers/mofotos');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, validateMofoto, isAuthor } = require('../middleware');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 router
   .route('/')
