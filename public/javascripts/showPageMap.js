efd2c7ad8f158018811e136b7a853cf2921bd991
Bonim <bonimouk@gmail.com>
2022-03-31 19:00:25 -0700

    Centering the Map on a Mofoto

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,6 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [-74.5, 40],
-  zoom: 9,
+  center: mofoto.geometry.coordinates,
+  zoom: 8,
 });
+
+new mapboxgl.Marker().setLngLat(mofoto.geometry.coordinates).addTo(map);
