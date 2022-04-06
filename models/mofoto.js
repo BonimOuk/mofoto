322a750adf47cc9d2dae4372eef11507c83d3294
Bonim <bonimouk@gmail.com>
2022-04-05 21:17:46 -0700

    Adding Custom Popups

--- models/mofoto.js
+++ models/mofoto.js
@@ -13,33 +13,45 @@ ImageSchema.virtual('thumbnail').get(function () {
   return this.url.replace('/upload', '/upload/w_200');
 });
 
-const MofotoSchema = new Schema({
-  title: String,
-  images: [ImageSchema],
-  geometry: {
-    type: {
-      type: String,
-      enum: ['Point'],
-      required: true,
+const opts = { toJSON: { virtuals: true } };
+
+const MofotoSchema = new Schema(
+  {
+    title: String,
+    images: [ImageSchema],
+    geometry: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
-  },
-  price: Number,
-  description: String,
-  location: String,
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  reviews: [
-    {
+    price: Number,
+    description: String,
+    location: String,
+    author: {
       type: Schema.Types.ObjectId,
-      ref: 'Review',
+      ref: 'User',
     },
-  ],
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Review',
+      },
+    ],
+  },
+  opts
+);
+
+MofotoSchema.virtual('properties.popUpMarkup').get(function () {
+  return `
+  <strong><a href="/mofotos/${this.id}">${this.title}</a></strong>
+  <p>${this.description.substring(0, 20)}...</p>
+  `;
 });
 
 MofotoSchema.post('findOneAndDelete', async function (doc) {
