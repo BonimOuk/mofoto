4782f3411a17fc4b59c4f30c4404626087bb4361
Bonim <bonimouk@gmail.com>
2022-06-13 07:42:25 -0700

    Using Helmet

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').config();
-// }
-
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const express = require('express');
 const path = require('path');
@@ -15,6 +13,8 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
+
 const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users');
@@ -54,8 +54,62 @@ const sessionConfig = {
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
+
 app.use(session(sessionConfig));
 app.use(flash());
+
+const scriptSrcUrls = [
+  'https://stackpath.bootstrapcdn.com/',
+  'https://api.tiles.mapbox.com/',
+  'https://api.mapbox.com/',
+  'https://kit.fontawesome.com/',
+  'https://cdnjs.cloudflare.com/',
+  'https://cdn.jsdelivr.net/',
+  'https://res.cloudinary.com/bonim/',
+];
+const styleSrcUrls = [
+  'https://kit-free.fontawesome.com/',
+  'https://stackpath.bootstrapcdn.com/',
+  'https://api.mapbox.com/',
+  'https://api.tiles.mapbox.com/',
+  'https://fonts.googleapis.com/',
+  'https://use.fontawesome.com/',
+  'https://cdn.jsdelivr.net/',
+  'https://res.cloudinary.com/bonim/',
+];
+const connectSrcUrls = [
+  'https://*.tiles.mapbox.com',
+  'https://api.mapbox.com',
+  'https://events.mapbox.com',
+  'https://res.cloudinary.com/bonim/',
+];
+const fontSrcUrls = ['https://res.cloudinary.com/bonim/'];
+
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [],
+        connectSrc: ["'self'", ...connectSrcUrls],
+        scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+        styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+        workerSrc: ["'self'", 'blob:'],
+        objectSrc: [],
+        imgSrc: [
+          "'self'",
+          'blob:',
+          'data:',
+          'https://res.cloudinary.com/bonim/', //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+          'https://images.unsplash.com/',
+        ],
+        fontSrc: ["'self'", ...fontSrcUrls],
+        mediaSrc: ['https://res.cloudinary.com/bonim/'],
+        childSrc: ['blob:'],
+      },
+    },
+    crossOriginEmbedderPolicy: false,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
