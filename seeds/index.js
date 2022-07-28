37825495f084966f06deea509786af0a00ea11e3
Bonim OUK <bonimouk@gmail.com>
2022-07-27 21:37:58 -0700

    Setting Up Mongo Atlas

--- seeds/index.js
+++ seeds/index.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
-const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers');
-const Mofoto = require('../models/mofoto');
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Mofoto = require("../models/mofoto");
 
-mongoose.connect('mongodb://localhost:27017/mofoto', {
+mongoose.connect("mongodb://localhost:27017/mofoto", {
   useNewUrlParser: true,
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Database connected');
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
 });
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
@@ -21,14 +21,14 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const mof = new Mofoto({
-      author: '621825a9116423323efc26c0',
+      author: "62e05998c0ff7b1192d6c685",
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, aperiam quibusdam! Velit voluptatem voluptatum doloribus illum quia possimus, earum dolores cupiditate ratione vitae tenetur molestiae, facere, fugiat iusto qui tempore!',
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, aperiam quibusdam! Velit voluptatem voluptatum doloribus illum quia possimus, earum dolores cupiditate ratione vitae tenetur molestiae, facere, fugiat iusto qui tempore!",
       price,
       geometry: {
-        type: 'Point',
+        type: "Point",
         coordinates: [
           cities[random1000].longitude,
           cities[random1000].latitude,
@@ -36,12 +36,12 @@ const seedDB = async () => {
       },
       images: [
         {
-          url: 'https://res.cloudinary.com/bonim/image/upload/v1647921130/Mofoto/fob2izgtuxdcwepjn8tf.jpg',
-          filename: 'Mofoto/fob2izgtuxdcwepjn8tf',
+          url: "https://res.cloudinary.com/bonim/image/upload/v1647921130/Mofoto/fob2izgtuxdcwepjn8tf.jpg",
+          filename: "Mofoto/fob2izgtuxdcwepjn8tf",
         },
         {
-          url: 'https://res.cloudinary.com/bonim/image/upload/v1647921130/Mofoto/ogvysl7f9pef0yqzzsbk.jpg',
-          filename: 'Mofoto/ogvysl7f9pef0yqzzsbk',
+          url: "https://res.cloudinary.com/bonim/image/upload/v1647921130/Mofoto/ogvysl7f9pef0yqzzsbk.jpg",
+          filename: "Mofoto/ogvysl7f9pef0yqzzsbk",
         },
       ],
     });
