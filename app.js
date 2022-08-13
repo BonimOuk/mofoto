4316653cd890eccfdb5c8e66d17c3bb092d16142
Bonim OUK <bonimouk@gmail.com>
2022-08-13 16:37:27 -0700

    Setting Up Mongo Atlas

--- app.js
+++ app.js
@@ -1,50 +1,53 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
-const session = require('express-session');
-const flash = require('connect-flash');
-const ExpressError = require('./utils/ExpressError');
-const methodOverride = require('method-override');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const User = require('./models/user');
-const helmet = require('helmet');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const helmet = require("helmet");
 
-const mongoSanitize = require('express-mongo-sanitize');
+const mongoSanitize = require("express-mongo-sanitize");
 
-const userRoutes = require('./routes/users');
-const mofotoRoutes = require('./routes/mofotos');
-const reviewRoutes = require('./routes/reviews');
+const userRoutes = require("./routes/users");
+const mofotoRoutes = require("./routes/mofotos");
+const reviewRoutes = require("./routes/reviews");
+// const dbUrl = process.env.DB_URL;
 
-mongoose.connect('mongodb://127.0.0.1:27017/mofoto', {
+// "mongodb://127.0.0.1:27017/mofoto"
+
+mongoose.connect("mongodb://127.0.0.1:27017/mofoto", {
   useNewUrlParser: true,
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Database connected');
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
 });
 
 const app = express();
 
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(mongoSanitize());
 
 const sessionConfig = {
-  name: 'session',
-  secret: 'thisismysecret',
+  name: "session",
+  secret: "thisismysecret",
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -59,31 +62,31 @@ app.use(session(sessionConfig));
 app.use(flash());
 
 const scriptSrcUrls = [
-  'https://stackpath.bootstrapcdn.com/',
-  'https://api.tiles.mapbox.com/',
-  'https://api.mapbox.com/',
-  'https://kit.fontawesome.com/',
-  'https://cdnjs.cloudflare.com/',
-  'https://cdn.jsdelivr.net/',
-  'https://res.cloudinary.com/bonim/',
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net/",
+  "https://res.cloudinary.com/bonim/",
 ];
 const styleSrcUrls = [
-  'https://kit-free.fontawesome.com/',
-  'https://stackpath.bootstrapcdn.com/',
-  'https://api.mapbox.com/',
-  'https://api.tiles.mapbox.com/',
-  'https://fonts.googleapis.com/',
-  'https://use.fontawesome.com/',
-  'https://cdn.jsdelivr.net/',
-  'https://res.cloudinary.com/bonim/',
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net/",
+  "https://res.cloudinary.com/bonim/",
 ];
 const connectSrcUrls = [
-  'https://*.tiles.mapbox.com',
-  'https://api.mapbox.com',
-  'https://events.mapbox.com',
-  'https://res.cloudinary.com/bonim/',
+  "https://*.tiles.mapbox.com",
+  "https://api.mapbox.com",
+  "https://events.mapbox.com",
+  "https://res.cloudinary.com/bonim/",
 ];
-const fontSrcUrls = ['https://res.cloudinary.com/bonim/'];
+const fontSrcUrls = ["https://res.cloudinary.com/bonim/"];
 
 app.use(
   helmet({
@@ -93,18 +96,18 @@ app.use(
         connectSrc: ["'self'", ...connectSrcUrls],
         scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
         styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-        workerSrc: ["'self'", 'blob:'],
+        workerSrc: ["'self'", "blob:"],
         objectSrc: [],
         imgSrc: [
           "'self'",
-          'blob:',
-          'data:',
-          'https://res.cloudinary.com/bonim/', //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
-          'https://images.unsplash.com/',
+          "blob:",
+          "data:",
+          "https://res.cloudinary.com/bonim/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+          "https://images.unsplash.com/",
         ],
         fontSrc: ["'self'", ...fontSrcUrls],
-        mediaSrc: ['https://res.cloudinary.com/bonim/'],
-        childSrc: ['blob:'],
+        mediaSrc: ["https://res.cloudinary.com/bonim/"],
+        childSrc: ["blob:"],
       },
     },
     crossOriginEmbedderPolicy: false,
@@ -121,35 +124,35 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
   console.log(req.query);
   res.locals.currentUser = req.user;
-  res.locals.success = req.flash('success');
-  res.locals.error = req.flash('error');
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
   next();
 });
 
-app.get('/fakeUser', async (req, res) => {
-  const user = new User({ email: 'steves@gmail.com', username: 'steves' });
-  const newUser = await User.register(user, 'stevessexy');
+app.get("/fakeUser", async (req, res) => {
+  const user = new User({ email: "steves@gmail.com", username: "steves" });
+  const newUser = await User.register(user, "stevessexy");
   res.send(newUser);
 });
 
-app.use('/', userRoutes);
-app.use('/mofotos', mofotoRoutes);
-app.use('/mofotos/:id/reviews', reviewRoutes);
+app.use("/", userRoutes);
+app.use("/mofotos", mofotoRoutes);
+app.use("/mofotos/:id/reviews", reviewRoutes);
 
-app.get('/', (req, res) => {
-  res.render('home');
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.all('*', (req, res, next) => {
-  next(new ExpressError('Page Not Found', 404));
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
 });
 
 app.use((err, req, res, next) => {
   const { statusCode = 500 } = err;
-  if (!err.message) err.message = 'Oh No, Something went wrong!';
-  res.status(statusCode).render('error', { err });
+  if (!err.message) err.message = "Oh No, Something went wrong!";
+  res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {
-  console.log('Serving on port 3000');
+  console.log("Serving on port 3000");
 });
